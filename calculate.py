6aa3e1062ac1ce555e63ea41f750cb0ada071803
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-04 11:56:00 -0600

    Added lines for restricting correct input data type and more specific returns for 0 denominators.

--- calculate.py
+++ calculate.py
@@ -7,11 +7,15 @@ def initialize():
     )
     parser.add_argument('-a',
                         '--value_a',
-                        help='The value of a'
+                        type=float,
+                        help='The value of a',
+                        required=True
     )
     parser.add_argument('-b',
                         '--value_b',
-                        help='The value of b'
+                        type=float,
+                        help='The value of b',
+                        required=True
     )
 
     args_parse = parser.parse_args()
@@ -20,8 +24,16 @@ def initialize():
 
 if __name__ == "__main__":
     args = initialize()
-    a = float(args.value_a)
-    b = float(args.value_b)
+    try: 
+        a = float(args.value_a)   # no need to do int(args.valu_a)
+    except:
+        print('The data type of a should be float or int.')
+
+    try:
+        b = float(args.value_b)  # no need to do int(args.valu_b)
+    except:
+        print('The data type of b should be float or int.')
+    
     result = math_lib.add(a, b) + math_lib.div(a, b) 
     print('The value of a: ', a)
     print('The value of b: ', b)
--- math_lib.py
+++ math_lib.py
@@ -1,9 +1,21 @@
 def div(a, b):
-    if b == 0:
-        return None
+    if not isinstance(a, float) and not isinstance(a, int):
+        return 'a and b must both be float or integer.'
+    if not isinstance(b, float) and not isinstance(b, int):
+        return 'a and b must both be float or integer.'
+    if a > 0 and b == 0:
+        return 'Inf'
+    elif a < 0 and b == 0:
+        return '-Inf'
+    elif a ==0 and b == 0:
+        return 'NaN'
     else:
         return a/b
 
 
 def add(a, b):
+    if not isinstance(a, float) and not isinstance(a, int):
+        return 'a and b must both be float or integer.'
+    if not isinstance(b, float) and not isinstance(b, int):
+        return 'a and b must both be float or integer.'
     return a + b

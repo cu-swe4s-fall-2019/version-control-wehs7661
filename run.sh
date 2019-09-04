da4ec4db1fb5fcc7015c47825695784aac509cf3
Wei-Tse Hsu <wehs7661@colorado.edu>
2019-09-04 01:03:36 -0600

    Added add function and created calculate.py and run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+chmod +x run.sh
+
+echo This bash script runs the Python code calculate.py with given values of a and b as 5.5 and 1.0, respectively.
+echo Running calculate.py ...
+echo Below is the output of calcuate.py:
+echo -------------------------------------------------------------------------------------------------------------
+
+python calculate.py -a 5.5 -b 1.0

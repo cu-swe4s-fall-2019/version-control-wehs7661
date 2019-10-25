f158d01c7518c538ae5e074a961fad7fc0ff6780
Wei-Tse Hsu <wehs7661@users.noreply.github.com>
2019-10-25 10:36:51 -0600

    Modified run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-chmod +x run.sh
-
 echo This bash script runs the Python code calculate.py with given values of a and b as 5.5 and 1.0, respectively.
 echo Running calculate.py ...
 echo Below is the output of calcuate.py:
